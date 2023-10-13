ede4e8adfdc942c43ab11ae6e5d4493d488b81f0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-13 19:42:00 -0400

    Restructured the app pathing further

--- webapp/src/app/App.tsx
+++ webapp/src/app/App.tsx
@@ -1,5 +1,5 @@
-import logo from "./assets/logo.svg";
-import {Counter} from "./redux/counter/Counter";
+import logo from "./res/logo.svg";
+import {Counter} from "../redux/counter/Counter";
 import "./App.css";
 
 function App() {
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
-import App from "./App";
+import App from "./app/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
