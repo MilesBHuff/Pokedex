be2a601d46d990cbecacf424bb0e8268069e3e2d
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-13 12:59:08 -0400

    Enhance ESLintRC with my Aethodian website's conf
    https://github.com/Aethodia/Website/blob/02f84b6f5d9a3d91ead6d78e18a16d9169c77a95/frontend/.eslintrc.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,38 @@
 'use strict';
 module.exports = {
     root: true,
+
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: '.',
+        project: 'tsconfig.json',
+        createDefaultProgram: true, //BUG:  Workaround for parserOptions.project not working correctly in editors (https://github.com/typescript-eslint/typescript-eslint/issues/864#issuecomment-523213273)
+        ecmaVersion: '2021',
+        sourceType: 'module',
+        ecmaFeatures: {
+            impliedStrict: true,
+        },
+    },
+    plugins: [
+        'react-refresh',
+    ],
+    
+    ignorePatterns: [
+        '.eslintrc.cjs',
+        'dist', 
+    ],
+
     env: {
         browser: true,
         esnext: true,
+        node: true,
     },
+    globals: {},
+
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
-    ],
-    ignorePatterns: [
-        'dist', 
-        '.eslintrc.cjs',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        'react-refresh',
     ],
     rules: {
         'react-refresh/only-export-components': [
@@ -24,4 +40,5 @@ module.exports = {
             { allowConstantExport: true },
         ],
     },
+    overrides: {},
 }
