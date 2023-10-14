31ca30cbda8081633b2239ca78360501c5f9ea95
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-14 14:13:13 -0400

    Navbar is functional.

--- frontend/app/src/layout/nav.tsx
+++ frontend/app/src/layout/nav.tsx
@@ -1,6 +1,12 @@
+import {Link} from 'react-router-dom';
+
 ////////////////////////////////////////////////////////////////////////////////
 export const Nav = () => (
     <nav>
-        Navigation
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/pokemon">Pokémon</Link></li>
+            <li><Link to="/error">404</Link></li>
+        </ul>
     </nav>
 );
