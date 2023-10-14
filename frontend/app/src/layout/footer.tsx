e57819876f90bb09efa19ed549fefb3c448b66af
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-14 14:32:13 -0400

    Populated the footer.

--- frontend/app/src/layout/footer.tsx
+++ frontend/app/src/layout/footer.tsx
@@ -1,6 +1,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 export const Footer = () => (
     <footer>
-        Footer
+        <p>
+            <span>
+                Copyright © 2023
+            </span> <a href="https://GitHub.com/MilesBHuff" title="Miles Bradley Huff">
+                Miles B Huff
+            </a>, <a href="https://GitHub.com/MilesBHuff/Pokedex/blob/main/LICENSE.txt" title="Lesser Affero General Public License (v3.0 or later)">
+                LAGPL3+
+            </a>.
+        </p>
+        <p>
+            <span>
+                <i>Pokédex</i>® and <i>Pokémon</i>® are registered trademarks of
+            </span> <a href="https://www.Nintendo.com/US" title="Nintendo of America Inc">
+                Nintendo
+            </a>.
+        </p>
     </footer>
 );
