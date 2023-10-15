5b7e9b1e57d43d476f14b9537330db9656081a4e
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-15 02:21:05 -0400

    Search bar "works" now, but doesn't yet search

--- frontend/app/src/widgets/search-bar.tsx
+++ frontend/app/src/widgets/search-bar.tsx
@@ -1,7 +1,32 @@
+import {ChangeEvent, FormEvent, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
 ////////////////////////////////////////////////////////////////////////////////
-export const SearchBar = () => (
-    <div className="search-bar">
-        <input type="text" />
-        <button>🔍</button>
-    </div>
-);
+export const SearchBar = () => {
+    
+    const navigate = useNavigate();
+    const [value, setValue] = useState('');
+    const [valid, setValid] = useState(false);
+
+    const validityRegex = /[A-Z\d]+/i;
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        setValid((event.target.value.match(validityRegex)?.length ?? NaN) > 0);
+    }
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        navigate('/pokemon'); //TODO: Specify which Pokémon in a URL parameter.
+    }
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    return (
+        <form className="search-bar" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search" value={value} onChange={handleChange}/>
+            <button type="submit" disabled={!valid}>🔍</button>
+        </form>
+    );
+};
