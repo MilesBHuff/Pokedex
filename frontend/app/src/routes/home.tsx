93285befa2c83596ed85cf20c79377d36c8daed5
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-14 23:37:29 -0400

    Add text to homepage

--- frontend/app/src/routes/home.tsx
+++ frontend/app/src/routes/home.tsx
@@ -1,6 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 export const Home = () => (
     <section>
-        Home
+        <h2>
+            Welcome to this unofficial Pokédex!
+        </h2>
+        <p>
+            Feel free to search for Pokémon by name using the searchbar in the top-right corner.
+        </p>
     </section>
 );
