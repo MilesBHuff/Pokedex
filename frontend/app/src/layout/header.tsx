f8c8c1f8b57b940728cf5f65a814cd9564c2ad6b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-15 03:24:36 -0400

    Change wording in titles

--- frontend/app/src/layout/header.tsx
+++ frontend/app/src/layout/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => (
     <header>
         <div className="brandmark">
             <img src="/favicon.svg"/>
-            <h1><Link to="/">Unofficial Pokédex</Link></h1>
+            <h1><Link to="/">Pokédex (unofficial)</Link></h1>
         </div>
         <SearchBar />
     </header>
--- frontend/app/src/routes/home.tsx
+++ frontend/app/src/routes/home.tsx
@@ -2,7 +2,7 @@
 export const Home = () => (
     <section>
         <h2>
-            Welcome to this unofficial Pokédex!
+            Welcome to my unofficial Pokédex!
         </h2>
         <p>
             Feel free to search for Pokémon by name using the searchbar in the top-right corner.
