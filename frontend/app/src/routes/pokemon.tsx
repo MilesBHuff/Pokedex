b4aaba388cdc72bcfd4fba945ad55eda933cfee0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-15 02:39:06 -0400

    Prepped the Pokémon page to use URL params

--- frontend/app/src/routes/pokemon.tsx
+++ frontend/app/src/routes/pokemon.tsx
@@ -1,6 +1,15 @@
+import {useSearchParams} from 'react-router-dom';
+
 ////////////////////////////////////////////////////////////////////////////////
-export const Pokemon = () => (
-    <section>
-        <p>Pokémon</p>
-    </section>
-);
+export const Pokemon = () => {
+    const [searchParams] = useSearchParams();
+    /** The Pokémon's National 'Dex Number */
+    const id = searchParams.get('id');
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    return (
+        <section>
+            <p>{id}</p>
+        </section>
+    );
+};
