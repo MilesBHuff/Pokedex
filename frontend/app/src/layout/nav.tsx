54e69294322145280927a2a72813c1f87eec58cc
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-14 23:29:36 -0400

    Improve usability of nav

--- frontend/app/src/layout/nav.tsx
+++ frontend/app/src/layout/nav.tsx
@@ -1,12 +1,34 @@
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 ////////////////////////////////////////////////////////////////////////////////
-export const Nav = () => (
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/pokemon">Pokémon</Link></li>
-            <li><Link to="/error">404</Link></li>
-        </ul>
-    </nav>
-);
+export const Nav = () => {
+    const location = useLocation();
+
+    const entries: Array<{
+        name: string,
+        path: string,
+    }> = [{
+        name: 'Home',
+        path: '/',
+    }, {
+        name: 'Pokémon',
+        path: '/pokemon',
+    }, {
+        name: '404',
+        path: '/error',
+    }]
+
+    return (
+        <nav>
+            <ul>{
+                entries.map(entry =>
+                    <li>{
+                        entry.path !== location.pathname
+                            ? <Link to={entry.path}>{entry.name}</Link>
+                            : <a className="current">{entry.name}</a>
+                    }</li>
+                )
+            }</ul>
+        </nav>
+    );
+};
