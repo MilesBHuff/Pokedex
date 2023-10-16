01a6626a1e9ec8d79d3b5c358216238fd21b4e3d
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-15 20:35:21 -0400

    Remove excess underline after clicking on H1

--- frontend/app/src/layout/header.tsx
+++ frontend/app/src/layout/header.tsx
@@ -2,12 +2,21 @@ import {SearchBar} from '@/widgets/search-bar';
 import {Link} from 'react-router-dom';
 
 ////////////////////////////////////////////////////////////////////////////////
-export const Header = () => (
-    <header>
-        <div className="brandmark">
-            <img src="/favicon.svg"/>
-            <h1><Link to="/">Pokédex (unofficial)</Link></h1>
-        </div>
-        <SearchBar />
-    </header>
-);
+export const Header = () => {
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    const handleClick = () => {
+        (document.activeElement as ((HTMLElement & Partial<HTMLInputElement>) | undefined))?.blur?.();
+    }
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    return (
+        <header>
+            <div className="brandmark">
+                <img src="/favicon.svg"/>
+                <h1><Link to="/" onClick={handleClick}>Pokédex (unofficial)</Link></h1>
+            </div>
+            <SearchBar />
+        </header>
+    );
+};
