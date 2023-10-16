2229076ed618e20700d7ba26d3fe33a601e95ff3
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-16 13:03:41 -0400

    Reworked results into table

--- frontend/app/src/routes/search.tsx
+++ frontend/app/src/routes/search.tsx
@@ -1,9 +1,10 @@
 import {useAppSelector} from '@/redux/hooks.ts';
-import {useState} from 'react';
-import {Link, useSearchParams} from 'react-router-dom';
+import {MouseEventHandler, useState} from 'react';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
 ////////////////////////////////////////////////////////////////////////////////
 export const Search = () => {
+    const navigate = useNavigate();
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     // Get and validate search parameter
@@ -56,19 +57,39 @@ export const Search = () => {
     }
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    /** If the row has an ID, then clicking it should take us to the info page about that ID. */
+    const handleRowClick: MouseEventHandler<HTMLTableRowElement> = event => {
+        let target = '';
+
+        const attributes = event.currentTarget.attributes;
+        for(const attribute of attributes) {
+            if(attribute.name === 'id') target = attribute.value;
+        }
+
+        if(target) navigate(`/pokemon?id=${target}`);
+    };
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     return (
         <section>
             <h2>Search for "{query}"</h2>
             {matches.length > 0
-                ? <ul>{
-                    matches.map((match, index) =>
-                        <li key={index}>
-                            <Link to={`/pokemon?id=${match.id}`}>
-                                {match.id} {match.name}
-                            </Link>
-                        </li>
-                    )
-                }</ul>
+                ? <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        matches.map(match =>
+                            <tr key={match.id} id={match.id.toString(10)} onClick={handleRowClick}>
+                                <td>{match.id}</td>
+                                <td>{match.name}</td>
+                            </tr>
+                        )
+                    }</tbody>
+                </table>
                 : <p>No matches!</p>
             }
         </section>
