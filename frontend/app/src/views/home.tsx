f6b0d2a7b7ff6b8d6abea9dd9e1eb627db8f44e8
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-17 13:31:34 -0400

    Added a way to view random Pokémon to the homepage

--- frontend/app/src/views/home.tsx
+++ frontend/app/src/views/home.tsx
@@ -1,16 +1,27 @@
 import {FunctionComponent} from 'react';
+import {Link} from 'react-router-dom';
 
 ////////////////////////////////////////////////////////////////////////////////
-export const Home: FunctionComponent = () => (
-    <section id="home">
-        <h2>
-            Welcome to my custom Pokédex!
-        </h2>
-        <p>
-            Feel free to search for Pokémon by name using the searchbar in the top-right corner.
-        </p>
-        <p className="notelet">
-            The source code for this application can be found <a href="https://github.com/MilesBHuff/Pokedex">here</a>.
-        </p>
-    </section>
-);
+export const Home: FunctionComponent = () => {
+
+    const totalPokemonInDex = 1017; // As of gen9.
+    const randomPokemonId = Math.floor(Math.random() * totalPokemonInDex);
+
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
+    return (
+        <section id="home">
+            <h2>
+                Welcome to my custom Pokédex!
+            </h2>
+            <p>
+                Feel free to search for Pokémon by name using the searchbar in the top-right corner.
+            </p>
+            <p>
+                Alternatively, <Link to={`/pokemon?id=${randomPokemonId}`}>click here</Link> to view a random Pokémon.
+            </p>
+            <p className="notelet">
+                The source code for this application can be found <a href="https://github.com/MilesBHuff/Pokedex">here</a>.
+            </p>
+        </section>
+    );
+};
