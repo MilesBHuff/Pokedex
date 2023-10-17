50ffa8507e09b00fd790bdcfee4e3a7d3a3a6a32
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-17 13:47:27 -0400

    Tidied a bit

--- frontend/app/src/views/home.tsx
+++ frontend/app/src/views/home.tsx
@@ -12,14 +12,11 @@ export const Home: FunctionComponent = () => {
         <section id="home">
             <h2>
                 Welcome to my custom Pokédex!
-            </h2>
-            <p>
+            </h2><p>
                 Feel free to search for Pokémon by name using the searchbar in the top-right corner.
-            </p>
-            <p>
+            </p><p>
                 Alternatively, <Link to={`/pokemon?id=${randomPokemonId}`}>click here</Link> to view a random Pokémon.
-            </p>
-            <p className="notelet">
+            </p><p className="notelet">
                 The source code for this application can be found <a href="https://github.com/MilesBHuff/Pokedex">here</a>.
             </p>
         </section>
