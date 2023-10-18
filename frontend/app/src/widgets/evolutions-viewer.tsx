f3ee034ba889b8f726692ffd8da02697f1455c0a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-18 17:02:30 -0400

    Separated out helper functions

--- frontend/app/src/widgets/evolutions-viewer.tsx
+++ frontend/app/src/widgets/evolutions-viewer.tsx
@@ -13,42 +13,6 @@ export const EvolutionsViewer: FunctionComponent<{evolutionId: number, speciesId
     // useEffect(() => console.debug(evolutions), [evolutions]);
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
-    /** Figures out whether the evolution chain is a line or a tree from the current ID. */
-    const calcLinearity = (
-        link: ChainLink,
-        id?: number | undefined,
-        isLinear: boolean = true,
-        idFound: boolean = false,
-    ): boolean => {
-        if(!isLinear) return isLinear; // If we've already found non-linearity, then return early to save on resources.
-
-        // Check to see if the current ID matchs the link ID;  if so, continue.
-        if(!idFound && isValidNumber(id)) {
-            if(id === urlToId(link.species.url)) {
-                idFound = true;
-            } else {
-
-                // Skip through the chain until we find the current ID;  trees can become linear on their branches.
-                for(let i = 0; i < link.evolves_to.length; i++) {
-                    if(id === urlToId(link.evolves_to[i]!.species.url)) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                        return calcLinearity(link.evolves_to[i]!, id, isLinear, idFound = true); //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                    }
-                }
-            }
-        }
-
-        // Check the length to ascertain linearity.
-        switch(link.evolves_to.length) {
-            case 0: // End of the line; no impact on linearity.
-                return isLinear;
-            case 1: // This link is linear, but the next one might not be.
-                return calcLinearity(link.evolves_to[0]!, id, isLinear, idFound); //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `0` is a known valid index for `link.evolves_to`.
-            default: // This link is non-linear.
-                return false;
-        }
-    };
-
-    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     /** Whether the evolution chain is a line or a tree. */
     const [isLinear, setIsLinear] = useState(true);
     const updateLinearity = (): void => {
@@ -70,4 +34,40 @@ export const EvolutionsViewer: FunctionComponent<{evolutionId: number, speciesId
             </span>
         ) : null}
     </>;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/** Figures out whether the evolution chain is a line or a tree from the current ID. */
+const calcLinearity = (
+    link: ChainLink,
+    id?: number | undefined,
+    isLinear: boolean = true,
+    idFound: boolean = false,
+): boolean => {
+    if(!isLinear) return isLinear; // If we've already found non-linearity, then return early to save on resources.
+
+    // Check to see if the current ID matchs the link ID;  if so, continue.
+    if(!idFound && isValidNumber(id)) {
+        if(id === urlToId(link.species.url)) {
+            idFound = true;
+        } else {
+
+            // Skip through the chain until we find the current ID;  trees can become linear on their branches.
+            for(let i = 0; i < link.evolves_to.length; i++) {
+                if(id === urlToId(link.evolves_to[i]!.species.url)) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                    return calcLinearity(link.evolves_to[i]!, id, isLinear, idFound = true); //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                }
+            }
+        }
+    }
+
+    // Check the length to ascertain linearity.
+    switch(link.evolves_to.length) {
+        case 0: // End of the line; no impact on linearity.
+            return isLinear;
+        case 1: // This link is linear, but the next one might not be.
+            return calcLinearity(link.evolves_to[0]!, id, isLinear, idFound); //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `0` is a known valid index for `link.evolves_to`.
+        default: // This link is non-linear.
+            return false;
+    }
 };
--- frontend/app/src/widgets/evolutions/evolution-line.tsx
+++ frontend/app/src/widgets/evolutions/evolution-line.tsx
@@ -9,64 +9,6 @@ import {Link} from 'react-router-dom';
 ////////////////////////////////////////////////////////////////////////////////
 /** Displays an evolution chain in the form of a line. */
 export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | undefined}> = props => {
-
-    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
-    /** Convert the chain into a line. */
-    const chainToLine = (
-        chain: ChainLink,
-        id?: number | undefined,
-    ): Array<BasicPokemonInfo> => {
-        const line: Array<BasicPokemonInfo> = [];
-
-        /** Add a chain link to the line. */
-        const addToLine = (
-            link: ChainLink,
-            idFound: boolean = false,
-        ): void => {
-
-            // Add the current link.
-            line.push({
-                id: urlToId(link.species.url),
-                name: link.species.name,
-            });
-
-            // Set the next link.
-            if(link.evolves_to.length < 1) return; // End of the line.
-            let nextInLine: ChainLink = link.evolves_to[0]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-
-            // If we haven't matched the ID yet, check the current ID.
-            if(!idFound) {
-                idFound = isValidNumber(id) && id === urlToId(link.species.url);
-            }
-
-            // If we've matched the ID before or now, then we can choose the next node at random, if there are more than one of them.
-            if(idFound) {
-                if(link.evolves_to.length > 1) { //WARN:  The below is only a rough approximation of tree evolutions;  please use `EvolutionTree` instead of `EvolutionLine` to properly display evolution trees.
-                    const index = Math.floor(Math.random() * link.evolves_to.length); // Choosing a random index to at least get some variety.
-                    nextInLine = link.evolves_to[index]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `index` is a known valid key for `link.evolves_to`.
-                }
-            } else {
-
-                // If even the current ID wasn't a match, try looking deeper
-                if(!idFound) {
-                    for(let i = 1; i < link.evolves_to.length; i++) {
-                        if(urlToId(link.evolves_to[i]!.species.url) === id) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                            idFound = true;
-                            nextInLine = link.evolves_to[i]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // Add the next link.
-            addToLine(nextInLine, idFound);
-        };
-        addToLine(chain);
-        return line;
-    };
-
-    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     const line = chainToLine(props.chain, props.id);
     return <>
         {line.map((link, index) => (
@@ -82,4 +24,60 @@ export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | u
             </Fragment>
         ))}
     </>;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/** Convert the chain into a line. */
+const chainToLine = (
+    chain: ChainLink,
+    id?: number | undefined,
+): Array<BasicPokemonInfo> => {
+    const line: Array<BasicPokemonInfo> = [];
+
+    /** Add a chain link to the line. */
+    const addToLine = (
+        link: ChainLink,
+        idFound: boolean = false,
+    ): void => {
+
+        // Add the current link.
+        line.push({
+            id: urlToId(link.species.url),
+            name: link.species.name,
+        });
+
+        // Set the next link.
+        if(link.evolves_to.length < 1) return; // End of the line.
+        let nextInLine: ChainLink = link.evolves_to[0]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+
+        // If we haven't matched the ID yet, check the current ID.
+        if(!idFound) {
+            idFound = isValidNumber(id) && id === urlToId(link.species.url);
+        }
+
+        // If we've matched the ID before or now, then we can choose the next node at random, if there are more than one of them.
+        if(idFound) {
+            if(link.evolves_to.length > 1) { //WARN:  The below is only a rough approximation of tree evolutions;  please use `EvolutionTree` instead of `EvolutionLine` to properly display evolution trees.
+                const index = Math.floor(Math.random() * link.evolves_to.length); // Choosing a random index to at least get some variety.
+                nextInLine = link.evolves_to[index]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `index` is a known valid key for `link.evolves_to`.
+            }
+        } else {
+
+            // If even the current ID wasn't a match, try looking deeper
+            if(!idFound) {
+                for(let i = 1; i < link.evolves_to.length; i++) {
+                    if(urlToId(link.evolves_to[i]!.species.url) === id) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                        idFound = true;
+                        nextInLine = link.evolves_to[i]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Add the next link.
+        addToLine(nextInLine, idFound);
+    };
+    addToLine(chain);
+    return line;
 };
