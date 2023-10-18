fab59548e14d9e59fdcc2887b7a0d52c75bf17f7
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-18 16:36:27 -0400

    Reintroduce random branching for treevolutions

--- frontend/app/src/widgets/evolutions/evolution-line.tsx
+++ frontend/app/src/widgets/evolutions/evolution-line.tsx
@@ -12,34 +12,62 @@ export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | u
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     /** Convert the chain into a line. */
-    const chainToLine = (chain: ChainLink): Array<BasicPokemonInfo> => {
+    const chainToLine = (
+        chain: ChainLink, 
+        id?: number | undefined,
+    ): Array<BasicPokemonInfo> => {
         const line: Array<BasicPokemonInfo> = [];
 
-        const addToLine = (link: ChainLink): void => {
+        /** Add a chain link to the line. */
+        const addToLine = (
+            link: ChainLink, 
+            idFound: boolean = false,
+        ): void => {
+
+            // Add the current link.
             line.push({
                 id: getIdFromUrl(link.species.url),
                 name: link.species.name,
             });
 
-            // Add the next node to the line.
+            // Set the next link.
             if(link.evolves_to.length < 1) return; // End of the line.
             let nextInLine: ChainLink = link.evolves_to[0]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-            if(isValidNumber(props.id)) {
-                for(let i = 1; i < link.evolves_to.length; i++) {
-                    if(getIdFromUrl(link.evolves_to[i]!.species.url) === props.id) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                        nextInLine = link.evolves_to[i]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
-                        break;
+
+            // If we haven't matched the ID yet, check the current ID.
+            if(!idFound) {
+                idFound = isValidNumber(id) && id === getIdFromUrl(link.species.url);
+            }
+            
+            // If we've matched the ID before or now, then we can choose the next node at random, if there are more than one of them.
+            if(idFound) {
+                if(link.evolves_to.length > 1) { //WARN:  The below is only a rough approximation of tree evolutions;  please use `EvolutionTree` instead of `EvolutionLine` to properly display evolution trees.
+                    const index = Math.floor(Math.random() * link.evolves_to.length); // Choosing a random index to at least get some variety.
+                    nextInLine = link.evolves_to[index]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `index` is a known valid key for `link.evolves_to`.
+                }
+            } else {
+
+                // If even the current ID wasn't a match, try looking deeper
+                if(!idFound) {
+                    for(let i = 1; i < link.evolves_to.length; i++) {
+                        if(getIdFromUrl(link.evolves_to[i]!.species.url) === id) { //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                            idFound = true;
+                            nextInLine = link.evolves_to[i]!; //NOTE: Non-null assertion used to work around issue where TypeScript is unable to know that `i` is a known valid index for `link.evolves_to`.
+                            break;
+                        }
                     }
                 }
             }
-            addToLine(nextInLine);
+
+            // Add the next link.
+            addToLine(nextInLine, idFound);
         };
         addToLine(chain);
         return line;
     };
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
-    const line = chainToLine(props.chain);
+    const line = chainToLine(props.chain, props.id);
     return <>
         {line.map((link, index) => (
             <Fragment key={index}>
