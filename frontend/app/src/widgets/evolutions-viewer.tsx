b604e4be335ae09476cf8fbf80b1a1d7b2add62f
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2023-10-21 18:48:22 -0400

    Rewrote evolution line assemblage logic

--- frontend/app/src/widgets/evolutions-viewer.tsx
+++ frontend/app/src/widgets/evolutions-viewer.tsx
@@ -10,6 +10,7 @@ import {Link} from 'react-router-dom';
 ////////////////////////////////////////////////////////////////////////////////
 export const EvolutionsViewer: FunctionComponent<{evolutionId: number, speciesId?: number | undefined}> = props => {
 
+    //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     const {data: evolutions} = useEvolutionsByIdQuery(props.evolutionId);
     // useEffect(() => console.debug(evolutions), [evolutions]);
 
@@ -17,7 +18,7 @@ export const EvolutionsViewer: FunctionComponent<{evolutionId: number, speciesId
     return <>
         {evolutions ? (
             <span className="evolutions-viewer">
-                <EvolutionLine chain={evolutions.chain} id={props.speciesId} />
+                <EvolutionLine chainLink={evolutions.chain} speciesId={props.speciesId} />
             </span>
         ) : null}
     </>;
@@ -25,14 +26,14 @@ export const EvolutionsViewer: FunctionComponent<{evolutionId: number, speciesId
 
 ////////////////////////////////////////////////////////////////////////////////
 /** Displays an evolution chain in the form of a line. */
-export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | undefined}> = props => {
+const EvolutionLine: FunctionComponent<{chainLink: ChainLink, speciesId?: number | undefined}> = props => {
 
     //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
     const [line, setLine] = useState([] as Array<BasicPokemonInfo>);
     const [idValid, setIdValid] = useState(false);
     const onPropsChange = () => {
-        setLine(chainToLine(props.chain, props.id));
-        setIdValid(isValidNumber(props.id));
+        setLine(chainToLine(props.chainLink, props.speciesId));
+        setIdValid(isValidNumber(props.speciesId));
     }
     useEffect(onPropsChange, [props]);
 
@@ -40,7 +41,7 @@ export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | u
     return <>
         {line.map((link, index) => (
             <Fragment key={index}>
-                {idValid && link.id === props.id ? <>
+                {idValid && link.id === props.speciesId ? <>
                     {displayifyName(link.name)}
                 </> : <>
                     <Link to={`/pokemon?id=${link.id}`}>
@@ -54,60 +55,107 @@ export const EvolutionLine: FunctionComponent<{chain: ChainLink, id?: number | u
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-/** Convert the chain into a line. */
+/** Given a `ChainLink` and optionally a `speciesId`, build and return a valid linear evolution chain in the form of an ordered `Array`.
+ * @param chainLink The `ChainLink` you'd like to work from.
+ * @param [speciesId] The `speciesId` you'd like to build an `evolutionLine` for.
+ * @returns an ordered `Array` containing a linear evolution chain for a given `speciesId` (or random, if omitted).
+ */
 const chainToLine = (
-    chain: ChainLink,
-    id?: number | undefined,
+    chainLink: ChainLink,
+    speciesId?: number | undefined,
 ): Array<BasicPokemonInfo> => {
-    const line: Array<BasicPokemonInfo> = [];
+    const evolutionLine: Array<BasicPokemonInfo> = [];
 
-    /** Add a chain link to the line. */
-    const addToLine = (
-        link: ChainLink,
-        idFound: boolean = false,
-    ): void => {
+    // If an ID was passed, try to build up a line to it.
+    if(speciesId) {
+        const newChain = findIdInChain(chainLink, speciesId, evolutionLine);
 
-        // Add the current link.
-        line.push({
-            id: urlToId(link.species.url),
-            name: link.species.name,
-        });
+        // If the new chain is valid, save it over the old.
+        if(newChain) chainLink = newChain;
+    }
 
-        // Set the next link.
-        if(link.evolves_to.length < 1) return; // End of the line.
-        let nextInLine: ChainLink | undefined = undefined;
+    // Complete the evolution line.
+    completeEvolutionLine(chainLink, evolutionLine); //TODO: Do something with the return value from this.
+    return evolutionLine;
+}
 
-        // If we haven't matched the ID yet, check the current ID.
-        if(!idFound) {
-            idFound = isValidNumber(id) && id === urlToId(link.species.url);
-        }
+////////////////////////////////////////////////////////////////////////////////
+/** Find where a Pokémon is in a chain, and return the node that contains that Pokémon.
+ * @param chainLink The `ChainLink` to start from.
+ * @param speciesId The species ID for the Pokémon you're searching for.
+ * @param [evolutionLine] Where this function should store the path taken to get to the target `speciesId`.  Please ensure your line is an empty `Array`, for maximum sanity. 
+ * @param [evolutionLineProvided] Whether an `evolutionLine` was provided.  Used to avoid unnecessary `null`checks.  Please do not provide this parameter yourself.
+ * @returns the `ChainLink` that contained the species ID, or `null` if there were no matches.
+**/
+//FIXME: Doesn't work for branching evolutions.  :(
+const findIdInChain = (
+    chainLink: ChainLink,
+    speciesId: number,
+    evolutionLine?: Array<BasicPokemonInfo> | undefined,
+    evolutionLineProvided: boolean = !!evolutionLine,
+): ChainLink | null => {
+    const chainLinkSpeciesId = urlToId(chainLink.species.url);
 
-        // If even the current ID wasn't a match, try looking deeper
-        if(!idFound) {
-            for(let i = 0; i < link.evolves_to.length; i++) {
-                if(urlToId(link.evolves_to[i]!.species.url) === id) {
-                    idFound = true;
-                    nextInLine = link.evolves_to[i]!;
-                    break;
-                }
-            }
-            //BUG:  If the match is one level deeper, this won't find it, and we'll end-up going with a random index...
-            //TODO:  Make this recursive somehow, to fix the above bug.
-        }
+    // Assume that this is either the matching node *or* a step on the way to the matching node.
+    if(evolutionLineProvided) evolutionLine!.push({
+        name: chainLink.species.name, 
+        id: chainLinkSpeciesId,
+    });
+    
+    // If the current node matches, then return it.
+    if(chainLinkSpeciesId === speciesId) {
+        return chainLink;
+    }
 
-        // If we have no next-in-line at this point, then we can choose the next node at random.
-        if(!nextInLine) {
-            if(link.evolves_to.length > 1) { //WARN:  This is only a rough approximation of tree evolutions;  please use `EvolutionTree` instead of `EvolutionLine` to properly display evolution trees.
-                const index = Math.floor(Math.random() * link.evolves_to.length); // Choosing a random index to at least get some variety.
-                nextInLine = link.evolves_to[index]!;
-            } else {
-                nextInLine = link.evolves_to[0]!;
-            }
-        }
+    // Else, if there are no futher nodes to investigate, remove what we just added and return.
+    if(chainLink.evolves_to.length <= 0) {
+        if(evolutionLineProvided) evolutionLine!.pop();
+        return null;
+    }
 
-        // Add the next link.
-        addToLine(nextInLine, idFound);
-    };
-    addToLine(chain);
-    return line;
+    // Else, investigate further nodes.
+    for(const nextLink of chainLink.evolves_to) {
+        const result = findIdInChain(nextLink, speciesId, evolutionLine, evolutionLineProvided);
+        if(result) return result
+    }
+
+    // Fallback return, to please TypeScript.
+    return null;
 };
+
+////////////////////////////////////////////////////////////////////////////////
+/** Fill out a provided `evolutionsLine` with the rest of the evolution chain.
+ * @param chainLink The `ChainLink` to start from.
+ * @param evolutionLine Where to store the path taken through the chain.
+ * @param [depth] The number of times this function has recursed.  Please do not provide this paramter yourself.
+ * @param [branchingFound] Whether branching has been found.  Please do not provide this parameter yourself.
+ * @returns the depth at which the chain branches, or `0` if it remains linear.
+**/
+const completeEvolutionLine = (
+    chainLink: ChainLink,
+    evolutionLine: Array<BasicPokemonInfo>,
+    depth: number = 0,
+    branchingFound: boolean = false,
+): number => {
+    
+    // If there are no deeper `chainLink`s, then we're done!
+    if(chainLink.evolves_to.length <= 0) {
+        return branchingFound ? depth : 0;
+    }
+
+    // Check evolutions for branching.
+    branchingFound = chainLink.evolves_to.length > 1;
+
+    // If branching was found, set the `index` at random;  else, set it to `0`.
+    const index = branchingFound ? Math.floor(Math.random() * chainLink.evolves_to.length) : 0;
+    const nextChainLink = chainLink.evolves_to[index]!;
+
+    // Save the chosen `chainLink` in the `evolutionLine`.
+    evolutionLine.push({
+        name: nextChainLink.species.name, 
+        id: urlToId(nextChainLink.species.url),
+    });
+
+    // Visit the next chain link.
+    return completeEvolutionLine(nextChainLink, evolutionLine, depth + 1, branchingFound);
+}
